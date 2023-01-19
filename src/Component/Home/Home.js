1802597a0c481056bb748e2aa1dc01fb7a3af90d
MohammadSazzatulIslam <rockstarwds@gmail.com>
2023-01-19 13:00:02 +0600

    add loader

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import ScrollToTopOnMount from "../ScrollToTop/ScrollToTop";
 
 const Home = () => {
   return (
     <div>
+      <ScrollToTopOnMount></ScrollToTopOnMount>
       <h1>this is home page</h1>
     </div>
   );
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -6,15 +6,52 @@ const Header = () => {
 
   return (
     <div>
-      <nav className="w-full px-5 shadow">
+      <nav className="w-full px-5  shadow">
         <div className="justify-between  mx-auto items-center md:flex ">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               <Link to="/">
-                <h2 className="text-2xl uppercase font-bold text-white">
-                  Eco Garden
-                </h2>
+                <h2 className="text-2xl uppercase  font-bold">Eco Garden</h2>
               </Link>
+              <div className="md:hidden flex items-center gap-2">
+                <button className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800">
+                  Log In
+                </button>
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 "
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
           </div>
           <div>
@@ -25,22 +62,27 @@ const Header = () => {
             >
               <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
                 <Link to="/">
-                  <li className="text-white text-md font-semibold hover:text-indigo-200">
+                  <li className=" text-md font-semibold hover:text-blue-500">
                     Home
                   </li>
                 </Link>
                 <Link to="/about">
-                  <li className="text-white text-md font-semibold hover:text-indigo-200">
+                  <li className=" text-md font-semibold hover:text-blue-500">
                     About
                   </li>
                 </Link>
                 <Link to="/profile">
-                  <li className="text-white text-md font-semibold hover:text-indigo-200">
+                  <li className=" text-md font-semibold hover:text-blue-500">
                     Profile
                   </li>
                 </Link>
               </ul>
             </div>
+          </div>
+          <div className="hidden  lg:flex items-center space-x-2 md:inline-block">
+            <button className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800">
+              Log In
+            </button>
           </div>
         </div>
       </nav>
