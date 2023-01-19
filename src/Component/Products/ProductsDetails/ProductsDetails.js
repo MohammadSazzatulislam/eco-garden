dd556291b837e959a45f6991ec5c0f8fabfc3363
MohammadSazzatulIslam <rockstarwds@gmail.com>
2023-01-19 18:39:10 +0600

    update

--- src/Component/Products/ProductsDetails/ProductsDetails.js
+++ src/Component/Products/ProductsDetails/ProductsDetails.js
@@ -8,11 +8,11 @@ const ProductsDetails = () => {
   return (
     <div>
       <ScrollToTopOnMount></ScrollToTopOnMount>
-      <div className="2xl:container 2xl:mx-auto lg:py-10 lg:px-20 md:py-5 md:px-3 py-2 px-2 ">
+      <div className="container mx-auto p-5 ">
         <div className="flex justify-center items-center lg:flex-row flex-col-reverse gap-6">
           {/* <!--  Div --> */}
 
-          <div className="  w-full sm:w-96 md:w-8/12 lg:w-6/12 items-center">
+          <div className="  w-full  md:w-8/12 lg:w-6/12 items-center">
             <h2 className="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800 mt-4">
               {products.title}
             </h2>
@@ -22,7 +22,7 @@ const ProductsDetails = () => {
             <h1 className="font-semibold text-lg my-3  ">
               Benefits Of {products.title}
             </h1>
-            <div className="">
+            <div className="pl-5">
               {products.benefits?.map((detail, i) => (
                 <ul key={i} className="list-disc">
                   <li>{detail}</li>
@@ -33,7 +33,7 @@ const ProductsDetails = () => {
 
           {/* <!-- Preview Images Div For larger Screen--> */}
 
-          <div className=" w-full sm:w-96 md:w-8/12  lg:w-6/12 flex lg:flex-row flex-col lg:gap-8 sm:gap-3 gap-2">
+          <div className=" w-full  md:w-8/12  lg:w-6/12 flex lg:flex-row flex-col lg:gap-8 gap-2">
             <div className=" w-full bg-gray-100 flex justify-center items-center">
               <img className="h-full" src={products.img} alt="" />
             </div>
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>this is about page</h1>
+        <div className='flex flex-wrap p-5 '>
+            <p className='text-md '>This is a Fruits info application (SPA) . Fully Responsive (mobile , tablet, desktop first approce ). user see details of fruits benefits. and this application use some technology like , react , tailwind css, react icon etc . and owsam slider .</p>
         </div>
     );
 };
